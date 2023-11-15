d187a599dbc2f07434f5cd87ac01c440475b0899
Vese10 <146728778+Vese10@users.noreply.github.com>
2023-11-15 18:58:05 +0100

    ok

--- JS/index.js
+++ JS/index.js
@@ -2,9 +2,27 @@ const btn = document.querySelector('button');
 const div = document.querySelector('div');
 const paragrafo = document.createElement('p');
 
+/*
 function addParagrafo(){
   paragrafo.textContent = 'Elemento creato dinamicamente. ';
   div.appendChild(paragrafo);
 }
 
 btn.addEventListener('click', addParagrafo);
+*/
+
+
+
+function takeInfo(){
+  const city = document.querySelector('#cityInput').value;
+  
+
+  fetch(`https://api.teleport.org/api/urban_areas/slug:${city}/scores/`)
+  .then(response => response.json())
+  .then(json => {
+    classeFetch = json;
+    console.log(json.summary);
+  });
+}
+
+btn.addEventListener('click', takeInfo);
