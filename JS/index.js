709fc5ba9c58a01c465cfbe4ba2224834919960a
Vese10 <146728778+Vese10@users.noreply.github.com>
2023-11-24 17:49:30 +0100

    ok

--- JS/index.js
+++ JS/index.js
@@ -2,24 +2,30 @@ const btn = document.querySelector('button');
 const div = document.querySelector('div');
 
 function takeInfo() {
+
   let city = document.getElementById('cityInput').value.toLowerCase();
   const url = `https://api.teleport.org/api/urban_areas/slug:${city}/scores/`;
 
   fetch(url)
     .then(response => {
       if (!response.ok) {
-        throw new Error('Errore nella richiesta');
+        throw new Error('Request error');
       }
       return response.json();
     })
     .then(json => {
-      console.log(json.summary);
-      const paragrafo = document.createElement('p');
-      paragrafo.textContent = city;
-      div.appendChild(paragrafo);
+      const citySummary = document.createElement('p');
+      citySummary.textContent = json.summary;
+      div.appendChild(citySummary);
+      const cityScore = document.createElement('p');
+      cityScore.textContent = json.teleport_city_score;
+      div.appendChild(cityScore);
     })
     .catch(error => {
-      console.error('Si è verificato un errore durante la richiesta:', error);
+      console.error('There is an errore in the request:', error);
+      const cityNotFound = document.createElement('p');
+      cityNotFound.textContent = 'Your city is not in our database. Please try again.';
+      div.appendChild(cityNotFound);
     });
 }
 
