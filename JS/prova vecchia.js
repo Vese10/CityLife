a25053394ea47c650c61c43ba8f08d8964a703bf
Vese10 <146728778+Vese10@users.noreply.github.com>
2023-11-28 00:16:02 +0100

    prove fetch

--- JS/prova vecchia.js
+++ JS/prova vecchia.js
@@ -1,4 +1,3 @@
-/*
 const btn = document.querySelector('button');
 const div = document.querySelector('div');
 const cityName = document.createElement('p');
@@ -149,76 +148,4 @@ btn.addEventListener('click', function() {
   takeCity();
   takeInfo();
   takeCategories();
-});
-*/
-
-const btn = document.querySelector('button');
-const div = document.querySelector('div');
-
-function handleSuccess(json){
-  const cityNotFound = document.createElement('p');
-  cityNotFound.textContent = '';
-  div.appendChild(cityNotFound);
-
-  const cityName = document.createElement('p');
-  cityName.textContent = json._embedded["city:search-results"][0].matching_full_name;
-  div.appendChild(cityName);
-
-  const citySummary = document.createElement('p');
-  citySummary.textContent = json.summary
-  div.append(citySummary);
-
-  const cityScore = document.createElement('p');
-  cityScore.textContent = "The teleport city score is: " + json.teleport_city_score.toFixed(2) + " out of 100.";
-
-  json.categories.forEach(category => {
-    const categoryParagraph = document.createElement('p');
-    categoryParagraph.textContent = `${category.name}: ${category.score_out_of_10.toFixed(2)} out of 10.`;
-    div.appendChild(categoryParagraph);
-  });
-}
-
-function handleFailure(error){
-  const cityNotFound = document.createElement('p');
-  cityNotFound.textContent = 'Your city is not in our database. Please try again.';
-  div.appendChild(cityNotFound);
-}
-
-async function takeInfos() {
-  try {
-    let city = document.getElementById('cityInput').value.toLowerCase();
-
-    const urls = [
-      `https://api.teleport.org/api/cities/?search=${city}`,
-      `https://api.teleport.org/api/urban_areas/slug:${city}/scores/`
-    ];
-
-    const fetchData = async (url) => {
-      const response = await fetch(url);
-
-      if (!response.ok) {
-        throw new Error(`Failed to fetch data from ${url}`);
-      }
-
-      return response.json();
-    };
-
-    const results = await Promise.all(urls.map(url => fetchData(url)));
-
-    // Log the results to inspect the structure
-    console.log('Results:', results);
-
-    // Assuming the structure of the JSON response, adjust accordingly
-    handleSuccess({
-      cityInfo: results[0],
-      cityScores: results[1]
-    });
-  } catch (error) {
-    console.error('Error in takeInfos:', error);
-    handleFailure(error);
-  }
-}
-
-btn.addEventListener('click', function(){
-  takeInfos();
 });
