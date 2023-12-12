8f5c4d2d710fefdc7238335d40b9f308f8210c85
Vese10 <146728778+Vese10@users.noreply.github.com>
2023-12-12 18:29:09 +0100

    webpack added

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './JS/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
